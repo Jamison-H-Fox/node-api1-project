76d6c5b74aedb290da268df81bbc444bc41b2d8f
Jamison Fox <fox.jamison@outlook.com>
2023-01-27 13:12:04 -0700

    completed POST, DELETE and PUT request portion of MVP

--- api/server.js
+++ api/server.js
@@ -35,12 +35,66 @@ server.get('/api/users/:id', async (req, res) => {
 
 // POST //
 server.post('/api/users', async (req, res) => {
+    const { name, bio } = req.body
     try {
-        
+        if(!name || !bio){
+            res.status(400).json({
+                message: `Please provide name and bio for the user`
+            })
+        } else {
+            const newUser = await User.insert({ name, bio })
+            res.status(201).json(newUser)
+        }
     } catch (err) {
-        //
+        res.status(500).json({
+
+        })
     }
 })
 
+// DELETE //
+server.delete(`/api/users/:id`, async (req, res) => {
+    try {
+        const { id } = req.params
+        const deletedUser = await User.remove(id)
+        if(!deletedUser) {
+            res.status(404).json({
+                message: 'The user with the specified ID does not exist'
+            })
+        } else {
+            res.status(200).json(deletedUser)
+        }
+    } catch {
+        res.status(500).json({
+            message: `The user could not be removed`
+        })
+    }
+})
 
-module.exports = server; // EXPORT YOUR SERVER instead of {}
+// PUT //
+server.put(`/api/users/:id`, async (req, res) => {
+    try {
+        const { id } = req.params
+        const { name, bio } = req.body
+        if(!name || !bio) {
+            res.status(400).json({
+                message: 'Please provide name and bio for the user'
+            })
+        } else {
+            const updatedUser = await User.update(id, { name, bio })
+            if (!updatedUser) {
+                res.status(404).json({
+                    message: 'The user with the specified ID does not exist'
+                })
+            } else {
+                res.status(200).json(updatedUser)
+            }
+        }
+    } catch {
+        res.status(500).json({
+            message: 'The user information could not be modified'
+        })
+    }
+})
+
+module.exports = server; 
